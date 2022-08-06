c5f976e7a2aebc0eccfe5f70011f26f8a8c31fa5
Aleksey Kabanov <aleksey.kabanov@gmail.com>
2022-08-06 23:31:03 +0300

    Add resource_group in data path

--- main.tf
+++ main.tf
@@ -4,15 +4,15 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_service_plan" "sp" {
   name                = "demoAppServicePlan77"
-  location            = data.rg.location
-  resource_group_name = data.rg.name
+  location            = data.azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
   os_type             = "Linux"
   sku_name            = "S1"
 }
 
 resource "azurerm_linux_web_app" "app" {
   name                = "appService-3754"
-  resource_group_name = data.rg.name
+  resource_group_name = data.azurerm_resource_group.rg.name
   location            = azurerm_service_plan.sp.location
   service_plan_id     = azurerm_service_plan.sp.id
   application_stack {
