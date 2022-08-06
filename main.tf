b1e2d2535431142b1ddc4b00cd3962e46d100bf0
Aleksey Kabanov <aleksey.kabanov@gmail.com>
2022-08-06 23:20:42 +0300

    Add HCL infra files

---
+++ main.tf
@@ -0,0 +1,31 @@
+data "azurerm_resources" "rg" {
+  name = "serviceapp-rg"
+}
+
+resource "azurerm_service_plan" "sp" {
+  name                = "demoAppServicePlan77"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  os_type             = "Linux"
+  sku_name            = "S1"
+}
+
+resource "azurerm_linux_web_app" "app" {
+  name                = "appService-3754"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_service_plan.sp.location
+  service_plan_id     = azurerm_service_plan.sp.id
+  application_stack {
+    dotnet_version = "6.0"
+  }
+  site_config {}
+}
+
+resource "azurerm_linux_web_app_slot" "app_slot" {
+  name           = "appService-3754-slot"
+  app_service_id = azurerm_linux_web_app.app.id
+  application_stack {
+    dotnet_version = "6.0"
+  }
+  site_config {}
+}
